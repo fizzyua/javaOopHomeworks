6cb1999a66cb8c0c714cfbd00538f273d8c5089f
Mykhailo Romanko <fizzy@ua.fm>
2018-07-31 22:01:36 +0300

    lesson4 4/4

---
+++ lesson4/src/com/company/Human.java
@@ -0,0 +1,60 @@
+package com.company;
+
+public class Human {
+    private int age;
+    private String sex;
+    private String firstName;
+    private String lastName;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Human(int age, String sex, String firstName, String lastName) {
+        this.age = age;
+        this.sex = sex;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Human() {
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "age=" + age +
+                ", sex='" + sex + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
